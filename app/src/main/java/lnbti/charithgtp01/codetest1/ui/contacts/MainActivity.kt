94364338463b249804f953f355fdede733146918
Charith <vinodyav@tellko.com>
2023-08-22 00:44:25 +0530

    Create adapter and data binding

--- app/src/main/java/lnbti/charithgtp01/codetest1/ui/contacts/MainActivity.kt
+++ app/src/main/java/lnbti/charithgtp01/codetest1/ui/contacts/MainActivity.kt
@@ -6,18 +6,57 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import lnbti.charithgtp01.codetest1.R
 import lnbti.charithgtp01.codetest1.databinding.ActivityMainBinding
+import lnbti.charithgtp01.codetest1.model.Contact
 
 class MainActivity : ComponentActivity() {
 
     private var binding: ActivityMainBinding? = null
-    private lateinit var viewModel: ContactsViewModel
+    private lateinit var viewModel: ContactsListViewModel
+    private lateinit var contactsListAdapter: ContactsListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this)[ContactsViewModel::class.java]
+        viewModel = ViewModelProvider(this)[ContactsListViewModel::class.java]
         binding?.vm = viewModel
         binding?.lifecycleOwner = this
+
+        initiateAdapter()
+        viewModelObservers()
+
     }
+
+    /**
+     * Recycle View data configuration
+     */
+    private fun initiateAdapter() {
+        /* Initiate Adapter */
+        contactsListAdapter =
+            ContactsListAdapter(object : ContactsListAdapter.OnItemClickListener {
+                override fun itemClick(item: Contact) {
+                    when (item.name) {
+
+                    }
+                }
+            })
+
+        /* Set Adapter to Recycle View */
+        binding?.recyclerView.also { it2 ->
+            it2?.adapter = contactsListAdapter
+        }
+    }
+
+    /**
+     * Live Data Updates
+     */
+    private fun viewModelObservers() {
+        /* Observer to catch list data
+      * Update Recycle View Items using Diff Utils
+      */
+        viewModel.contactsList.observe(this) {
+            contactsListAdapter.submitList(it)
+        }
+    }
+
 }
 
